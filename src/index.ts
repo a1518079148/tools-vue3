48b21044c3d2704a35b16b3c932354a090b76d98
1518079148 <1518079148@qq.com>
2023-03-03 12:22:22 +0800

    更新通讯

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import AxiosBean, { AxiosBeanRes } from './AxiosBean'
+import AxiosBean, { AxiosBeanRes } from './axios/AxiosBean'
 import CookieUtil from './CookieUtil'
 import CVue3 from './CVue3'
 import FileUtil from './FileUtil'
 import ObjectUtil from './ObjectUtil'
 import TimeUtil from './TimeUtil'
 import { DateType } from './types/common'
+import WebSocketBean from './websocket/WebSocketBean'
 
-export { CVue3, CookieUtil, ObjectUtil, TimeUtil, FileUtil, AxiosBean, type AxiosBeanRes, DateType }
+export { CVue3, CookieUtil, ObjectUtil, TimeUtil, FileUtil, AxiosBean, WebSocketBean, type AxiosBeanRes, DateType }
